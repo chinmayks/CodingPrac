1e5938162d3dd3bd80ed59fe925a2bc018b523ea
Chinmay <chinmayks@github.com>
2018-05-26 19:37:50 -0400

    bfs implemented

--- src/Algo/Tree/BreadthFirstSearch.java
+++ src/Algo/Tree/BreadthFirstSearch.java
@@ -1,13 +1,46 @@
 package Algo.Tree;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * 
+ * @author {Chinmay Singh}
+ * 26-May-2018
+ * BreadthFirstSearch.java
+ */
+
 public class BreadthFirstSearch {
-	
+	/**
+	 * 
+	 *  @param node
+	 *  @return void
+	 */
 	public void bfs(BinaryTree node) {
-		
+		if(!node.isEmpty()) {
+			Queue<Treenode> queue = new LinkedList<Treenode>();
+			Treenode root = node.getRoot();
+			queue.add(node.getRoot());
+			while(!queue.isEmpty()) {
+				root = (Treenode) queue.poll();
+				System.out.println(root.getNode());
+				if(root.getLeftChild() != null){
+                	queue.add(root.getLeftChild());
+                }
+				if(root.getRightChild() != null){
+					queue.add(root.getRightChild());
+                }
+				
+							
+                
+			}
+		}
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		BinaryTree btree = new CreateBtree().createTree();
+		BreadthFirstSearch bfs = new BreadthFirstSearch();
+		bfs.bfs(btree);
 		
 
 	}
--- src/Algo/Tree/CreateBtree.java
+++ src/Algo/Tree/CreateBtree.java
@@ -3,7 +3,7 @@ package Algo.Tree;
 public class CreateBtree {
 	public BinaryTree createTree() {
 		BinaryTree btree = new  BinaryTree();
-        int[] bst = new int[]{10,9,11,-5};
+        int[] bst = new int[]{10,9,11,-5, 11, 12};
         for(int tree_val : bst){
             btree.insert(tree_val);
         }
